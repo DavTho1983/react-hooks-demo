ee681e223843634a2c2d93556db9681bc8a1ad76
Melanie Freshness <david.alex.thompson@gmail.com>
2019-11-06 20:43:52 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Login from "./User/Login";
+import Logout from "./User/Logout";
+import Register from "./User/Register";
+import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
+
+  function handleChange(evt) {
+    setName(evt.target.value);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My name is {name}</h1>
+      <input type="text" onChange={handleChange} value={name} />
+      <Login></Login>
+      <Logout></Logout>
+      <Register></Register>
     </div>
   );
 }
